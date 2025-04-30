0042a016fb097f57dfbcd3551cb6d9326ff5a02b
Cedarich <godwinekoh@gmail.com>
2025-04-30 15:46:43 +0100

    ✨ Implement on-chain payment confirmation with webhooks

--- src/blockchain/blockchain.module.ts
+++ src/blockchain/blockchain.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { CacheModule } from '@nestjs/cache-manager'; // <-- Add this line
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { StellarService } from './services/stellar.service';
+import { WebhookService } from './services/webhook.service';
 import { BlockchainController } from './controllers/blockchain.controller';
-import { HorizonService } from './services/horizon/horizon.service';
+import { WebhookController } from './controllers/webhook.controller';
+import { Transaction } from '../transactions/entities/transaction.entity';
 
 @Module({
-  imports: [ConfigModule, CacheModule.register()], // <-- Add CacheModule here
-  providers: [StellarService, HorizonService],
-  controllers: [BlockchainController],
-  exports: [StellarService, HorizonService], // <-- Export services if needed in other modules
+  imports: [ConfigModule, TypeOrmModule.forFeature([Transaction])],
+  providers: [StellarService, WebhookService],
+  controllers: [BlockchainController, WebhookController],
+  exports: [StellarService, WebhookService],
 })
 export class BlockchainModule {}

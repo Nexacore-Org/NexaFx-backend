73c5f0546d8a53bbd8c0e9ec5244fceb39a8434c
Yusuf Falade <faladeyusuf54@gmail.com>
2025-06-07 06:01:37 +0100

    Created the profile picture feature

---
+++ src/profile-picture/profile-picture.module.ts
@@ -0,0 +1,15 @@
+import { Module } from "@nestjs/common"
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { MulterModule } from "@nestjs/platform-express"
+import { ProfilePictureController } from "./profile-picture.controller"
+import { ProfilePictureService } from "./profile-picture.service"
+import { ProfilePicture } from "./entities/profile-picture.entity"
+import { multerConfig } from "./config/multer.config"
+
+@Module({
+  imports: [TypeOrmModule.forFeature([ProfilePicture]), MulterModule.register(multerConfig)],
+  controllers: [ProfilePictureController],
+  providers: [ProfilePictureService],
+  exports: [ProfilePictureService],
+})
+export class ProfilePictureModule {}

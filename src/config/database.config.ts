00e803cfa779086ac3adb7dcd8ae48871dac91bf
portable <emmanueltemitopedorcas20@gmail.com>
2025-06-27 00:28:44 +0100

    fix(database): correct environment variable for database name

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -6,7 +6,7 @@ export default registerAs('database', () => ({
   port: parseInt(process.env.DB_PORT || '', 10) || 5432,
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'byteacademy',
+  database: process.env.DB_DATABASE || 'byteacademy',
   synchronize: process.env.DB_SYNCHRONIZE === 'true',
   ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
 }));

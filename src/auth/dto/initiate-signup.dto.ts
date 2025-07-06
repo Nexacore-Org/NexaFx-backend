01ffbc0027e16e0703950069e83a8029436651ed
portable <emmanueltemitopedorcas20@gmail.com>
2025-07-06 21:46:42 +0100

    mail

---
+++ src/auth/dto/initiate-signup.dto.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength, IsPhoneNumber } from 'class-validator';
+
+export class InitiateSignupDto {
+  @ApiProperty({ example: 'user@example.com' })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({ example: '+1234567890' })
+  @IsPhoneNumber()
+  phone: string;
+
+  @ApiProperty({ example: 'StrongPassword123!' })
+  @IsString()
+  @MinLength(8)
+  password: string;
+}

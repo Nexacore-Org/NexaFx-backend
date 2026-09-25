32a02e073deb77a91e249c9e36c0e6a72306d9bb
Debbys-design <Debbys-design@users.noreply.github.com>
2026-09-25 19:11:01 +0000

    feat(abuse-prevention): implement persisted abuse flagging, throttling, and admin review flow
    Closes #1043

--- src/abuse-prevention/abuse-prevention.module.ts
+++ src/abuse-prevention/abuse-prevention.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AbusePreventionController } from './abuse-prevention.controller';
 import { AbusePreventionService } from './abuse-prevention.service';
+import { AbuseFlag } from './entities/abuse-flag.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AbuseFlag])],
   controllers: [AbusePreventionController],
   providers: [AbusePreventionService],
   exports: [AbusePreventionService],
